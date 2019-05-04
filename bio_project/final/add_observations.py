1a55c32170ba9547c52d65992c3a6522005df0d7
Matthew Murphy <mjm052@uark.edu>
2019-05-04 14:54:15 -0500

    Added option to add weather info to the database.

--- bio_project/final/add_observations.py
+++ bio_project/final/add_observations.py
@@ -6,10 +6,10 @@ import argparse
 affirmation = [Y, y, YES, Yes, yes]
 declination = [N, n, NO, No, no]
 
-# define functions for defining data to add to the db
-	
+# define functions for identifying data to be added:
 def add_species()
 	# Add species one by one
+	i = int()
 	i = 0
 	print("Now add species observations:")
 	while (i<1):
@@ -25,9 +25,31 @@ def add_species()
 			i = 0
 		else:
 			i += 1
+			species = {
+				"name" = name,
+				"n" = n
+			}
+			return(species)
 
-# define function for adding observation (species) to db:
-def connector_input():
+def add_weather()
+	# Add weather information
+	print("Now add weather for the current observation:")
+	cloud_cover = input("Cloud cover: ")
+	precipitation = input("Describe precipitation: ")
+	temperature = input("Temperature (in deg. C): ")
+	wind_speed = input("Wind speed (in km/h): ")
+	data_source = input("Where did you get these data? ")
+	weather = {
+		"cloud_cover" : cloud_cover,
+		"precipitation" : precipitation,
+		"temperature" : temperature,
+		"wind_speed" : wind_speed,
+		"data_source" : data_source
+	}
+	return(weather)
+
+# define function for committing data to the database:
+def input_species(species):
 	cnx = mysql.connector.connect(user='mjmurphy', database='butterfly')
 	cursor = cnx.cursor()
 
@@ -36,18 +58,30 @@ def connector_input():
 	               "(date, site, name, n) "
 	               "VALUES (%s, %s, %s, %s, %s)")
 
+	insert_observation = species
 	# begin insert observation_book
 	cursor.execute(add_observation)
 
+	# commit new data and close the connection
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+def input_weather(weather):
+	cnx = mysql.connector.connect(user='mjmurphy', database='butterfly')
+	cursor = cnx.cursor()
+
+	# set up new observation
+	insert_weather = ("INSERT INTO observation_book "
+	               "(date, cloud_cover, precipitation, temperature, wind_speed, data_source)"
+	               "VALUES (%s, %s, %s, %s, %s, %s)")
+
+	# begin insert observation_book
+	cursor.execute(insert_weather)
 
 	# add observation data
-	data_salary = {
-	  'date': date,
-	  'site': site,
-	  'name': name,
-	  'n': n,
-	}
-	cursor.execute(add_salary, data_salary)
+	
+	cursor.execute(insert_weather)
 
 	# commit new data and close the connection
 	cnx.commit()
