76d716be964407a6211c32aa4c9d4544c8de8a7c
Matthew Murphy <mjm052@uark.edu>
2019-05-05 15:22:42 -0500

    Fixed add_species function

--- bio_project/final/add_observations.py
+++ bio_project/final/add_observations.py
@@ -7,29 +7,40 @@ affirmation = [Y, y, YES, Yes, yes]
 declination = [N, n, NO, No, no]
 
 # define functions for identifying data to be added:
-def add_species()
+def add_species(*args):
 	# Add species one by one
 	i = int()
 	i = 0
 	print("Now add species observations:")
 	while (i<1):
-		sp_name = input("Species name: ")
-		sp_n = input("# observed: ")
-		name.extend(name,sp_name)
-		n.extend(n,sp_n)
-		print("species" + "\t" + "n")
-		for j in name:
-			print(name[j] + ": " + n[j])
-		more_test = input("Any more species to add? (Y/n")
-		if more_test in affirmation:
-			i = 0
+		# for a single species, input the name and # observed:
+		sp_name = ""
+		sp_name0 = input("Species name: ")
+		for x in sp_name0:
+			sp_name += x
+		sp_n = ""
+		sp_n0 = input("# observed: ")
+		for x in sp_n0:
+			sp_n += x
+
+		# extend the name and number lists:
+		name.append(sp_name)
+		n.append(sp_n)
+
+		# print off the list of all species and numbers:
+		# ask whether this is all
+		more_test = input("Any more species to add? (Y/n) ")
+
+		# if so, then keep the loop open
+		if(more_test == 'Y'):
+			i=0
+
+		# if not, close the loop and return the values
 		else:
-			i += 1
-			species = {
-				"name" = name,
-				"n" = n
-			}
-			return(species)
+			i+=1
+			#print(name)
+			#print(n)
+			return(name,n)
 
 def add_weather()
 	# Add weather information
@@ -48,8 +59,28 @@ def add_weather()
 	}
 	return(weather)
 
+def add_notes()
+	# Add optional notes for this entry
+	i = int()
+	ans = chr()
+	i = 0
+	while (i<1):
+		note = input("Notes for this entry: /n")
+		print("Your notes for this entry: /n" note)
+		ans = input("Is this correct?")
+		if ans in affirmation:
+			i += 1
+			return(note)
+		elif ans in declination:
+			i = 0
+			print("Note deleted; re-enter your notes ...")
+		else:
+			print("Error! Re-enter your notes.")
+			i = 0
+
+
 # define function for committing data to the database:
-def input_species(species):
+def input_species(species)
 	cnx = mysql.connector.connect(user='mjmurphy', database='butterfly')
 	cursor = cnx.cursor()
 
@@ -67,7 +98,7 @@ def input_species(species):
 	cursor.close()
 	cnx.close()
 
-def input_weather(weather):
+def input_weather(weather)
 	cnx = mysql.connector.connect(user='mjmurphy', database='butterfly')
 	cursor = cnx.cursor()
 
@@ -88,24 +119,43 @@ def input_weather(weather):
 	cursor.close()
 	cnx.close()
 
-print("Now to add new observations")
+def input_notes(note)
+	cnx = mysql.connector.connect(user='mjmurphy', database='butterfly')
+	cursor = cnx.cursor()
 
+	# set up new observation
+	add_notes = ("INSERT INTO note_book "
+	               "(ID,note)"
+	               "VALUES (%s, %s)")
+	
+	insert_notes = note
+	# begin insert note_book
+	cursor.execute(insert_notes)
+
+	# add observation data
+	cursor.execute(insert_notes)
+
+	# commit new data and close the connection
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+# add object names ...
+print("Now to add new observations")
+name = []
+n = []
 date = input("Date collected ( format: YYYY-MM-DD ): ")
 site = input("Field site where collected: ")
 
-print("You collected these data on " + data + "at " + site + "\n\n\n")
-ans = input("Is this correct? (Y/n")
+print("You collected these data on " + date + " at " + site + "\n\n\n")
+ans = input("Is this correct? (Y/n): ")
 
-if ans in affirmation:
-	add_species()
-elif ans in declination:
-	# this will be fixed ... (loop back to re-enter)
-else:
-	# return an error and loop back
+add_species()
+
+print(name)
+print(n)
 
 # add the observations
 print("Adding observation(s) ... ")
-add_observation(date, site, name, n)
+# add_observation(date, site, name, n)
 print("Observation(s) added!")
-
-ans = str()
